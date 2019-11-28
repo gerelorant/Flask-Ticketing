017844004d8665ed0de68b511dae1cf138970545
Gere Lóránt <gerelorant@gmail.com>
2019-11-28 15:04:10 +0100

    Initial commit

---
+++ flask_ticketing/__init__.py
@@ -0,0 +1,2 @@
+from flask_ticketing.core import Ticketing
+from flask_ticketing.model import TicketMixin, TicketTypeMixin
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Flask-Ticketing',
+    version='0.1',
+    packages=['flask_ticketing'],
+    url='https://github.com/gerelorant/Flask-Ticketing',
+    license='MIT',
+    author='Gere Lóránt',
+    author_email='gerelorant@gmail.com',
+    description='Ticketing extension for Flask'
+)
